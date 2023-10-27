f14b9ce020f30a4230d749c8efa56fedd54bb340
Haoyu Joshua Fan <89876991+haoyujfan@users.noreply.github.com>
2023-10-26 23:32:08 -0500

    skeleton multiplayer functions

--- Assignment4/start_menu.cs
+++ Assignment4/start_menu.cs
@@ -3,6 +3,45 @@ using System;
 
 public partial class start_menu : Node2D
 {
+	public override void _Ready() {
+		Multiplayer.PeerConnected += PeerConnected;
+		Multiplayer.PeerDisconnected += PeerDisconnected;
+		Multiplayer.ConnectedToServer += ConnectedToServer;
+		Multiplayer.ConnectionFailed += ConnectionFailed;
+	}
+
+	// runs when a player connects (runs on all peers)
+	// id - id of player that connected
+	private void PeerConnected(long id) {
+
+	}
+
+	// runs when a peer disconnects (runs on all peers)
+	// id - id of player that disconnected
+	private void PeerDisconnected(long id) {
+
+	}
+
+	// runs when the connection is successful (only runs on clients)
+	private void ConnectedToServer() {
+
+	}
+
+	// runs when connection fails (only runs on clients)
+	private void ConnectionFailed() {
+		
+	}
+	
+	private void _on_host_button_down()
+	{
+		// Replace with function body.
+	}
+
+	private void _on_join_button_down()
+	{
+		// Replace with function body.
+	}
+
 	private void _on_start_pressed()
 	{
 		GetTree().ChangeSceneToFile("res://scenes/main.tscn");
@@ -13,3 +52,5 @@ public partial class start_menu : Node2D
 		GetTree().Quit();
 	}
 }
+
+
