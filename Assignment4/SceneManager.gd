d8d2896276e00f4b051b24ec100ca889f9703bc8
mtnp <Silasp0801@gmail.com>
2023-11-02 19:37:09 -0500

    cleaned up lives labels

--- Assignment4/SceneManager.gd
+++ Assignment4/SceneManager.gd
@@ -6,11 +6,13 @@ var Effects : RichTextLabel
 var Rotate : RichTextLabel
 var Music : RichTextLabel
 var PointsValue : int
+var numPlayers : int
 var Player1 : Player
 var Player2 : Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	numPlayers = 1
 	PointsValue = 0
 	var index = 0
 	for i in GameManager.Players:
@@ -22,21 +24,22 @@ func _ready():
 			Player1 = currentPlayer
 		else:
 			Player2 = currentPlayer
+			numPlayers += 1
 		for spawn in get_tree().get_nodes_in_group("SpawnPoint"):
 			if spawn.name == "Spawn" + str(index):
 				currentPlayer.global_position = spawn.global_position
+		print(numPlayers)
 		index += 1
-
-	# Lives = get_node<RichTextLabel>("Lives") # No need to initialize RichTextLabels in GDScript
-	# Effects = get_node<RichTextLabel>("SoundEffectToggle")
-	# Rotate = get_node<RichTextLabel>("RotateToggle")
-	# Music = get_node<RichTextLabel>("MusicToggle")
-
 
 func _process(delta):
 	# Points.Text = "Points: " + get_node<Player>("Player").get_points() # If Points is a RichTextLabel
-	$Lives.text = "Player 1 Lives: " + str(Player1.get_lives())
-	$Lives2.text = "Player 2 Lives: " + str(Player2.get_lives())
+	if (numPlayers == 2):
+		$Lives.text = "Player 1 Lives: " + str(Player1.get_lives())
+		$Lives2.position = Vector2(883, 600)
+		$Lives2.text = "Player 2 Lives: " + str(Player2.get_lives())
+	
+	else:
+		$Lives.text = "Lives: " + str(Player1.get_lives())
 #
 #func _on_player_interact_orange():
 #	PointsValue += 1
